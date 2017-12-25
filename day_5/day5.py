6137068d5664b936ba73af7799cc3ea57bcb1271
Joachim Strömbergson <joachim@secworks.se>
2017-12-25 19:07:36 +0100

    Adding correct code for part one. Adding code for part two. Adding test code that verifies correctly for part two. Still getting wrong result for part two.

--- day_5/day5.py
+++ day_5/day5.py
@@ -18,45 +18,99 @@ import sys
 VERBOSE = 1
 
 
+#-------------------------------------------------------------------
+# get_input()
+#-------------------------------------------------------------------
+def get_input():
+    code = []
+    with open('my_input.txt','r') as f:
+        for line in f:
+            code.append(int(line.strip()))
+    return code
+
 
 #-------------------------------------------------------------------
+# part_one()
+# Execute the jumps in the given code including that we increase
+# the offset in each jump instruction that we executed.
+#
+# Exit the code when the jump ends up outside of the code.
+# Print how many cycles it took.
 #-------------------------------------------------------------------
-def part_one():
-    print("Part one.")
+def part_one(code):
+    print("Part one:")
+    ctr = 0
+    pc = 0
+    codelen = len(code)
+    offset = 0
+    while pc >= 0 and pc < codelen:
+        offset = code[pc]
+        code[pc] = code[pc] + 1
+        pc = pc + offset
+        ctr += 1
+
+    print("Exited code at pc = %d after %d cycles." % (pc, ctr))
     print()
 
 
 #-------------------------------------------------------------------
+# part_two()
+# Execute the jumps in the given code including that we either
+# increase or decrease the offset in each jump instruction
+# that we executed.
+#
+# Exit the code when the jump ends up outside of the code.
+# Print how many cycles it took.
 #-------------------------------------------------------------------
-def part_two():
-    print("Part two.")
+def part_two(code):
+    print("Part two:")
+    ctr = 0
+    pc = 0
+    codelen = len(code)
+    offset = 0
+
+    while pc >= 0 and pc < codelen:
+        offset = code[pc]
+        print("pc = %d, offset = %d" % (pc, offset))
+
+        if offset >= 3:
+            code[pc] = code[pc] - 1
+        else:
+            code[pc] = code[pc] + 1
+
+        pc = pc + offset
+        ctr += 1
+
+    print("Exited code at pc = %d after %d cycles." % (pc, ctr))
     print()
+    return (code, ctr)
 
 
 #-------------------------------------------------------------------
-# test_one()
-#-------------------------------------------------------------------
-def test_one():
-    print("Tests for part one:")
-    print()
-
-
-#-------------------------------------------------------------------
-# test_two()
 #-------------------------------------------------------------------
 def test_two():
-    print("Tests for part two:")
-    print()
+    code = [0, 3, 0, 1, -3]
+    ref = ([2, 3, 2, 3, -1], 10)
+
+    print("test code for part two:")
+    print("code before execution:")
+    print(code)
+    code2 = part_two(code)
+    print("code after execution:")
+    print(code2)
+
+    if code2 == ref:
+        print("Correct result after execution.")
 
 
 #-------------------------------------------------------------------
 # main()
 #-------------------------------------------------------------------
 def main():
-    test_one()
     test_two()
-    part_one()
-    part_two()
+#    my_code = get_input()
+#    part_one(my_code)
+#    part_two(my_code)
 
 
 #-------------------------------------------------------------------
