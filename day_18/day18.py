10e7778a238252a24194443e1b91ba0ff8b64b99
Joachim Strömbergson <joachim@secworks.se>
2018-01-17 19:22:41 +0100

    Added code for extracting register names and creating an intialized regfile.

--- day_18/day18.py
+++ day_18/day18.py
@@ -38,11 +38,24 @@ def load_sw():
 
 
 #-------------------------------------------------------------------
+#-------------------------------------------------------------------
+def build_regfile(sw):
+    regfile = {}
+    for instr in sw:
+        if len(instr) == 3:
+            (opcode, reg, value) = instr
+        else:
+            (opcode, reg) = instr
+        if reg.isalpha():
+            regfile[reg] = 0
+    return regfile
+
+#-------------------------------------------------------------------
 # main()
 #-------------------------------------------------------------------
 def main():
     my_program = load_sw()
-    print(my_program)
+    my_regfile = build_regfile(my_program)
 
 
 #-------------------------------------------------------------------
